526adc81e5e4d6eef6fdcc0b4a89320bf318c48a
colajam93 <fs.fake.g1@gmail.com>
2017-10-15 04:04:51 +0900

    Update string utils
    Add stob (string to bool) and join

--- include/String.hpp
+++ include/String.hpp
@@ -1,12 +1,36 @@
 #pragma once
 
+#include <algorithm>
+#include <iterator>
+#include <sstream>
 #include <string>
 
-namespace vrlk {
+namespace vrlk::String {
 
-bool startsWidth(const std::string& s, const std::string& prefix)
+inline bool startsWidth(const std::string& s, const std::string& prefix)
 {
     return s.compare(0, prefix.size(), prefix) == 0;
 }
 
-} // namespace vrlk
+inline bool stob(const std::string& s)
+{
+    return !(s == "false" || s.empty());
+}
+
+inline std::string join(const std::string& delimiter,
+                        const std::vector<std::string> v)
+{
+    if (v.empty()) {
+        return "";
+    }
+
+    std::ostringstream ss;
+    ss << v[0];
+
+    for (auto it = ++(std::begin(v)); it != std::end(v); ++it) {
+        ss << delimiter << *it;
+    }
+    return ss.str();
+}
+
+} // namespace vrlk::String
