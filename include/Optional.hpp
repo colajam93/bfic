1a2711d553332b11d4ed0843653fe15d26fadfc9
colajam93 <fs.fake.g1@gmail.com>
2017-10-15 04:03:59 +0900

    Change namespace naming

--- include/Optional.hpp
+++ include/Optional.hpp
@@ -4,23 +4,23 @@
 
 #include <optional>
 
-namespace vrlk {
+namespace vrlk::Optional {
 template <typename T>
 using Optional = std::optional<T>;
 
 static auto&& NullOpt = std::nullopt;
-} // namespace vrlk
+} // namespace vrlk::Optional
 
 #elif __has_include(<experimental/optional>)
 
 #include <experimental/optional>
 
-namespace vrlk {
+namespace vrlk::Optional {
 template <typename T>
 using Optional = std::experimental::optional<T>;
 
 static auto&& NullOpt = std::experimental::nullopt;
-} // namespace vrlk
+} // namespace vrlk::Optional
 
 #else
 #error "There is no optional implementation"
